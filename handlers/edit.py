3703bb35021785fe747a9db8f356a6094687871c
xselid <gavrilovnikita720@gmail.com>
2026-05-17 18:55:28 +0500

    fix mesages

--- handlers/edit.py
+++ handlers/edit.py
@@ -56,7 +56,7 @@ async def text_edit(bm: Message, bot: Bot, repo: Repo) -> None:
     text = _("<tg-emoji emoji-id=\"5301173701323028420\">✏️</tg-emoji> <b>Edit  ·  <a href='{user_link}'>{name}</a></b>"
              "\n\n<i>Was</i>"
              "<blockquote expandable>{old_msg}</blockquote>"
-             "<i>Now</i>"
+             "\n\n<i>Now</i>"
              "<blockquote expandable>{new_msg}</blockquote>",
              locale=user.language).format(user_link=user_link,
                                           name=bm.chat.full_name,
@@ -92,7 +92,7 @@ async def media_edit(bm: Message, bot: Bot, repo: Repo) -> None:
     text = _("<tg-emoji emoji-id=\"5301173701323028420\">✏️</tg-emoji> <b>Edit  ·  <a href='{user_link}'>{name}</a></b>"
              "\n\n<i>Was</i>"
              "<blockquote expandable>{old_msg}</blockquote>"
-             "<i>Now</i>"
+             "\n\n<i>Now</i>"
              "<blockquote expandable>{new_msg}</blockquote>",
              locale=user.language).format(user_link=user_link,
                                           name=bm.chat.full_name,
